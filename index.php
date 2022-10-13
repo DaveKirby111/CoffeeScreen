a379f0c67577915888363078d4cb7da48a25f2d9
David Kirby <davidkirby@Davids-MBP.attlocal.net>
2022-10-13 15:50:28 -0500

    working on styling and js

---
+++ index.php
@@ -0,0 +1,252 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> 
+        <title>Kinetic Kegerator</title>
+        <link rel="stylesheet" href="styles.css">
+        <script src="js/time.js"></script>
+        <script src="js/day.js"></script>
+        <script src="js/date.js"></script>
+    </head>
+    <body>
+
+    <div class="header">
+
+        <div class="date-cont">
+
+        <div class="thedate">
+
+        <p id="day"></p>
+        <p id="date"></p>
+
+        </div>
+
+        </div>
+
+        <div class="logo">
+
+        <a href="index.html">
+        <img src="images/kegerator-logo copy.png" alt="Kinetic">
+        </a>
+
+        </div>
+
+        <div class="time-cont">
+
+        <p id="time"></p>
+
+        </div>
+
+    </div>
+
+    <div class="bg"></div>
+
+    <div class="main">
+
+        <div class="content">
+
+    <div class="brewed">
+
+        <p>brewed at</p>
+        <p id="brew-time">placeholder</p>
+        
+    </div>
+
+    <div class="slider">
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Coffee dates back to 800 A.D.</p>
+
+        </div>
+
+
+        <div class="item">
+
+                <p>Fun Fact:</p>
+                <p>Coffee beans are seeds from cherry-like berries</p>
+
+        </div>
+
+                
+
+        <div class="item">
+
+                <p>Fun Fact:</p>
+                <p>You can eat coffee beans</p>
+
+        </div>
+
+                    
+
+        <div class="item">
+        
+                <p>Fun Fact:</p>
+                <p>There are two types of coffee beans, Arabica and Robusta</p>
+        
+        </div>
+
+                        
+
+        <div class="item">
+            
+                <p>Fun Fact:</p>
+                    <p>Brazil produces a third of the world's coffee</p>
+            
+        </div>
+
+                            
+
+        <div class="item">
+                
+                <p>Fun Fact:</p>
+                <p>Hawaii and California are the only two U.S. states that produce coffee</p>
+                
+        </div>
+
+                                
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Espresso means "pressed out" in Italian, referring to the way it is made</p>
+
+        </div>
+
+                                    
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>The world's most expensive coffee can cost more than $600 per pound</p>
+
+        </div>
+
+                                        
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Multiple people have tried to ban coffee thoughout history</p>
+
+        </div>
+
+                                            
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>You can overdose on coffee by drinking at least 30 cups in a short period of time</p>
+
+        </div>
+
+                                                
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>The average person in Finland goes through 27.5 pounds of coffee per year</p>
+
+        </div>
+
+                                                    
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Research has found drinking 3 to 4 cups per day leads to a longer lifespan</p>
+
+        </div>
+
+                                                        
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>The world record for the largest cup of coffee is 9 feet tall</p>
+
+        </div>
+
+                                                            
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Coffee was popularized in America by the Boston Tea Party</p>
+
+        </div>
+
+                                                                
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Decaf contains 2 to 12 milligrams of caffeine</p>
+
+        </div>
+
+                                                                    
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>The word coffee comes from the Arabic word wine</p>
+
+        </div>
+
+                                                                        
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Starbucks opens about 2 stores per day on average</p>
+
+        </div>
+
+                                                                            
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>1 cup of black coffee = 1 calorie</p>
+
+        </div>
+
+                                                                                
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>It is reported that Teddy Roosevelt coined Maxwell House's slogan</p>
+
+        </div>
+
+                                                                                    
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>There are 25,000 ways to order coffee at Dunkin' Doughnuts</p>
+
+        </div>
+
+        
+
+        <div class="item">
+
+            <p>Fun Fact:</p>
+            <p>Coffee grounds act as exfoliators that can lift dead skin cells</p>
+
+        </div>
+
+    </div>
+
+
+            </div>
+
+        </div>
+
+    </body>
+</html>
